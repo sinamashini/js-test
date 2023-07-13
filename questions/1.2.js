ed1cb65f858248259d01dc46d1dbe04c33471377
Sina Mashini <54537775+sinamashini@users.noreply.github.com>
2023-07-13 16:20:41 +0330

    Update 1.2.js
    comments are absolute hell

--- questions/1.2.js
+++ questions/1.2.js
@@ -9,7 +9,7 @@ const promisesOrder = () => {
     // Comments make code dirty based on the clean code which is fine for readablity
     // but I need to show off it's a interview so:
     // I don't use async & await sugar syntaxex here,
-    // which I found them sweet enough besides, then catch finnaly is a bit dirty most of the time but somtimes they are cleaner and sweeter javascript is gift and course u I beth u knew it:)
+    // which I found them sweet enough besides, then catch finnaly is a bit dirty most of the time but somtimes they are cleaner and sweeter. javascript is gift and course I bett u knew it:)
     
     promiseToShow.then((result) => {
         console.log(result);
